570f6b2b8542d01cb7bd42ee25eafcb35b1a786b
Guillaume ALAMOME <guillaume.alamome@airbus.com>
2023-10-03 11:11:48 +0200

    Client, et API fonctionnent

---
+++ fooapi/src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const cors = require("cors");
+app.use(cors());
+
+const serverName = "foo-api";
+
+app.get('/', (req, res) => {
+  return res.status(200).send({ "message": `Response from ${serverName}!` });
+});
+
+app.get('/app', (req, res) => {
+  return res.status(200).send({ "message": `App Response from ${serverName}!` });
+});
+
+app.get('/app/v1', (req, res) => {
+  return res.status(200).send({ "message": `App v1 Response from ${serverName}!` });
+});
+
+app.listen(5001, () => console.log('Server is up and running'));
